5437fcdf08ceb1c032234bc3390b46830be353ea
MehakChhabra-18 <mehakchhabra751@gmail.com>
2026-09-20 11:33:31 +0530

    fifo implemented

--- js/script.js
+++ js/script.js
@@ -30,6 +30,27 @@ simulateButton.addEventListener("click", function () {
             .split(" ")
             .map(Number);
 
+    if (algorithm === "fifo") {
+
+    const result = fifo(referencePages, frameCount);
+
+    console.log("FIFO Result:", result);
+
+    document.getElementById("pageFaults").textContent =
+        result.pageFaults;
+
+    document.getElementById("pageHits").textContent =
+        result.pageHits;
+
+    const totalPages = referencePages.length;
+
+    const hitRatio =
+        (result.pageHits / totalPages) * 100;
+
+    document.getElementById("hitRatio").textContent =
+        hitRatio.toFixed(2) + "%";
+}
+
 
     console.log("Reference Pages:", referencePages);
 
@@ -38,3 +59,42 @@ simulateButton.addEventListener("click", function () {
     console.log("Algorithm:", algorithm);
 
 });
+
+function fifo(referencePages,frameCount)
+{
+    const frames=[];
+    const history=[];
+    let pageFaults=0;
+    let pageHits=0;
+    let fifoIndex=0;
+    for(let page of referencePages)
+    {
+        let result;
+        if(frames.includes(page))
+        {
+            pageHits++;
+            result="Hit";
+        }
+        else
+        {
+            pageFaults++;
+            result="Fault";
+            if(frames.length<frameCount)
+            {
+                frames.push(page);
+            }
+            else
+            {
+                frames[fifoIndex]=page;
+                fifoIndex=(fifoIndex+1)%frameCount;
+            }
+        }
+
+        history.push({page:page,
+            frames:[...frames],
+            result:result
+        });
+    }
+
+    return {pageFaults,pageHits,history};
+}
