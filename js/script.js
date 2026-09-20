c59ee8b7993bbbfae7ccbf8061906b27598a3434
MehakChhabra-18 <mehakchhabra751@gmail.com>
2026-09-20 09:34:33 +0530

    Created page replacement interface

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 console.log("OS Lab JavaScript is running...");
+
+const simulateButton =
+    document.getElementById("simulateBtn");
+
+const referenceInput =
+    document.getElementById("referenceString");
+
+const frameInput =
+    document.getElementById("frameCount");
+
+const algorithmInput =
+    document.getElementById("algorithm");
+
+
+simulateButton.addEventListener("click", function () {
+
+    const referenceString =
+        referenceInput.value;
+
+    const frameCount =
+        Number(frameInput.value);
+
+    const algorithm =
+        algorithmInput.value;
+
+
+    const referencePages =
+        referenceString
+            .split(" ")
+            .map(Number);
+
+
+    console.log("Reference Pages:", referencePages);
+
+    console.log("Number of Frames:", frameCount);
+
+    console.log("Algorithm:", algorithm);
+
+});
